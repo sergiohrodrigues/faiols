b7386db36651a521e6b08c5b74449522f1fbc570
Tiago Queiroz <Tiago.dev.queiroz@gmail.com>
2025-10-08 23:19:41 -0300

    Criado Crontroller; Configurado SwaggerAnotations; Especificado itens do Program;

---
+++ Source/EstudosSolution.SeparadorBoletos.Api/Swagger/SwaggerAnotations.cs
@@ -0,0 +1,16 @@
+namespace EstudosSolution.SeparadorBoletos.Api.Swagger;
+
+public class SwaggerAnotations
+{
+    #region TAGS
+
+    public const string TAG_UPLOAD_BOLETOS = "1 - Upload de Boletos";
+
+    #endregion
+
+    #region DESCRICAO
+
+    public const string DESC_UPLOAD_BOLETOS = "Realiza o upload de boletos, que serão processados e classificados automaticamente com base nas respectivas datas de vencimento.";
+
+    #endregion
+}
